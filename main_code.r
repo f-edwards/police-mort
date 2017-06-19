139894199500b5e35a3127543523665704cc3fdd
Frank Edwards <fedwards@uw.edu>
2017-06-19 11:17:28 -0700

    fix white model

--- main_code.r
+++ main_code.r
@@ -152,7 +152,7 @@ blk.stan = stan_glmer(d.black ~ ur.code + (1|division),
 #                data = tmp1, offset=log(white), verbose=TRUE, control=glmerControl(optimizer="bobyqa",
 #                                                                                   optCtrl=list(maxfun=2e5)))
 
-wht.stan = stan_glmer(d.black ~ ur.code + (1|division),
+wht.stan = stan_glmer(d.white ~ ur.code + (1|division),
                       prior_intercept=normal((log(0.37)-log(100000)), 10), #for prior intercept, based on krieger estimates
                       prior = normal(0, 2.5), #weakly informative, no difference from big urban
                       prior_covariance = decov(1, 1, 1, 1), #default
