d4f519917354d8c92fbd6adb4ce125e6e4b3a731
Frank Edwards <fedwards@uw.edu>
2017-06-19 13:48:41 -0700

    add descriptive and model tables

--- tables.r
+++ tables.r
@@ -7,7 +7,6 @@ library(rstanarm)
 library(dplyr)
 library(tidyr)
 library(xtable)
-library(pander)
 load("models.RData")
 
 
@@ -48,6 +47,38 @@ maketable<-function(x, label){
   return(tab)
 }
 
+### set up newdata for ppd - use 100,000,000 for offset to get enough trials, 
+### then rescale to rate per 100,000 
+# ... blacks 
+
+ur.code  = unique(tmp2$ur.code)
+division = unique(tmp2$division)
+ur.division = expand.grid(division, ur.code); 
+names(ur.division) = c("division", "ur.code")
+n = length(division)*length(ur.code)
+
+newdata<-tmp2[1:n,]
+newdata$fips<-0; newdata$tot.pop<-newdata$black<-newdata$white<-newdata$latino<-100000000; 
+newdata$ur.code<-ur.division$ur.code; newdata$division<-ur.division$division
+
+post.blk<-posterior_predict(blk.stan, newdata=newdata)/1000
+post.blk.int<-bind_cols(ur.division, 
+                        as.data.frame(t(apply(post.blk, 2, function(x)quantile(x, probs=c(0.025, 0.5, 0.975))))))
+post.blk.int = post.blk.int %>% mutate(race = 'Black')
+
+# .. whites 
+post.wht<-posterior_predict(wht.stan, newdata=newdata)/1000
+post.wht.int<-bind_cols(ur.division, 
+                        as.data.frame(t(apply(post.wht, 2, function(x)quantile(x, probs=c(0.025, 0.5, 0.975))))))
+
+post.wht.int = post.wht.int %>% mutate(race = 'White')
+
+# ... latinx 
+post.lat<-posterior_predict(lat.stan, newdata=newdata)/1000
+post.lat.int<-bind_cols(ur.division,
+                        as.data.frame(t(apply(post.lat, 2, function(x)quantile(x, probs=c(0.025, 0.5, 0.975))))))
+post.lat.int = post.lat.int %>% mutate(race = 'Latino')
+
 
 tab.out<-cbind(maketable(post.blk.int, "Rate-Black"),
                maketable(post.lat.int, "Rate-Latino")[,2],
@@ -55,16 +86,13 @@ tab.out<-cbind(maketable(post.blk.int, "Rate-Black"),
 names(tab.out)<-c("County Name", "Black", "Latino", "White")
 
 
-Here is a ridiculous table with posterior estimates of mortality by race, region and metro type.
-
-{r, results="asis", message=FALSE, echo=FALSE}
-
 print(xtable(tab.out,
              caption="Posterior police related mortality estimates by race/ethnicity, census region, and metro type, 95 percent credible intervals"),
       include.rownames=FALSE,
-      size="\\fontsize{9pt}{10pt}\\selectfont",
       caption.placement="top",
-      comment=FALSE
+      type="html",
+      comment=FALSE,
+      file="estimates.html"
 )
 
 
@@ -81,10 +109,5 @@ names(tabUR)<-c("Division", "Large Central Metro", "Large Fringe Metro",
                 "Medium Metro", "Small Metro", "Micropolitan", "Noncore", "Total")
 
 tab.out<-xtable(tabUR, caption = "Police related fatalities in the U.S. by metro type and Census divsiion, 1/1/2013 - 5/8/2017")
-print(tab.out, type="html")
-
-
-#### also make an appx table for 
-
-
-
+print(tab.out, type="html", 
+      file="raw-counts.html")
