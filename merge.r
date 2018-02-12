f79752303409bd5dc3310080651ca64c4bd40aa7
Frank Edwards <fre9@cornell.edu>
2018-02-12 15:06:25 -0500

    beginning of updated block group imputation

--- merge.r
+++ merge.r
@@ -1,138 +1,177 @@
 rm(list=ls())
-library(dplyr)
+library(tidyverse)
 library(ggplot2)
 library(date)
 library(lubridate)
-library(data.table)
 library(stringr)
-library(tidyr)
 library(wru)
+library(jsonlite)
 
-setwd("C:/sync/police-mort/")
+fe<-read_csv("./data/fatal-encounters-5-8-17.csv")
+fe_new<-read_csv("./data/fatal-encounters-2-12-18.csv")
 
-fe<-fread("fatal-encounters-5-8-17.csv", header=TRUE)
+####### run script to link lat/long -> FIPS block
+files<-list.files("./data")
+if(!("block_map.csv"%in%files)){
+  coords<-fe_new%>%
+    select(Latitude, Longitude)%>%
+    mutate("FIPS_block" = NA,
+           "FIPS_county" = NA,
+           "FIPS_state" = NA,
+           "STname" = NA,
+           "API_status" = NA)
+  for(i in 1:nrow(coords)){
+    url<-paste("https://geo.fcc.gov/api/census/block/find?latitude=",
+               coords[i, 1], 
+               "&longitude=", 
+               coords[i, 2], 
+               "&showall=true&format=json",
+               sep="")
+    
+    temp<-fromJSON(url)
+    print(i)
+    
+    coords[i, 3:7]<-c(temp$Block$FIPS,
+                      temp$County$FIPS,
+                      temp$State$FIPS,
+                      temp$State$code,
+                      temp$status)
+    
+  }
+  write.csv(coords, "./data/block_map.csv", row.names=FALSE)
+}
 
-names(fe)<-c("id", "name", "age", "gender", "race", "image", "date", 
-             "address", "city", "state", "zip", "county", "agency", "cause",
-             "circumstance", "disposition", "news", "symptoms", "uniqueid",
-             "data-status", "date-descript", "dum", "dum2", "year")
+#### get block-level data from census api
+census_dat<-get_census_data(key="518b6e66ffa1857a9e4ffd5d894f2934bb06c045",
+                          states=unique(coords$STname),
+                          age=TRUE, sex=TRUE, census.geo="block")
 
-fe$county<-tolower(fe$county)
-
-
-fe$county<-gsub("city of st. louis", "st. louis city", fe$county)
-fe$county<-gsub("saint louis city", "st. louis city", fe$county)
-fe$county<-gsub("st .louis", "st. louis city", fe$county)
-fe$county<-gsub("city of st .louis", "st. louis city", fe$county)
-
-fe$county<-gsub("lasalle", "la salle", fe$county)
-fe$county<-gsub("prince georges", "prince george's", fe$county)
-fe$county<-gsub("mcclennan", "mclennan", fe$county)
-fe$county<-gsub("do単a ana", "dona ana", fe$county)
-fe$county<-gsub("yukon koyukuk", "yukon-koyukuk", fe$county)
-fe$county<-gsub("city of baltimore", "baltimore", fe$county)
-fe$county<-gsub("charelston", "charleston", fe$county)
-fe$county<-gsub("athens-clarke", "clarke", fe$county)
-fe$county<-gsub("prince of wales hyder", "prince of wales-hyder", fe$county)
-fe$county<-gsub("laporte", "la porte", fe$county)
-fe$county<-gsub("saint joseph", "st. joseph", fe$county)
-fe$county<-gsub("brooklyn", "kings", fe$county)
-fe$county<-gsub("clallam bay", "clallam", fe$county)
-fe$county<-gsub("saint mary's", "st. mary's", fe$county)
-
-fe$county<-gsub("city of newport news", "newport news city", fe$county)
-
-fe$county<-gsub("kuai", "kauai", fe$county)
-fe$county<-gsub("shively", "jefferson", fe$county)
-fe$county<-gsub("saint clair", "st. clair", fe$county)
-fe$county<-gsub("clarl", "clark", fe$county)
-fe$county<-gsub("park hill", "denver", fe$county)
-fe$county<-gsub("braziria", "brazoria", fe$county)
-fe$county<-gsub("andalusia", "covington", fe$county)
-fe$county<-gsub("oskaloosa", "okaloosa", fe$county)
-fe$county<-gsub("du page", "dupage", fe$county)
-fe$county<-gsub("comanche `", "comanche", fe$county)
-fe$county<-gsub("philadephia", "philadelphia", fe$county)
-fe$county<-gsub("oglala lakota", "shannon", fe$county)
-fe$county<-gsub("montrose county", "montrose", fe$county)
-fe$county<-gsub("dekalb", "de kalb", fe$county)
-fe$county<-gsub("prince george's", "prince georges", fe$county)
-fe$county<-gsub("east baton rouge", "e. baton rouge", fe$county)
-fe$county<-gsub("west baton rouge", "w. baton rouge", fe$county)
-
-fe$county<-gsub("lasalle", "la salle", fe$county)
-fe$county<-gsub("queen anne's", "queen annes", fe$county)
-fe$county<-gsub("matanuska susitna", "matanuska-susitna", fe$county)
-fe$county<-gsub("district of columbia", "the district", fe$county)
-fe$county<-gsub("dc", "the district", fe$county)
-
-fe$county<-gsub("st. john the baptist", "st. john baptist", fe$county)
-fe$county<-gsub("desoto", "de soto", fe$county)
-fe$county<-gsub("dupage", "du page", fe$county)
-fe$county<-gsub("yukon koyukuk", "yukon-koyukuk", fe$county)
-fe$county<-gsub("la paz", "lapaz", fe$county)
-fe$county<-gsub("mcdonough", "mc donough", fe$county)
-fe$county<-gsub("do単a ana", "dona ana", fe$county)
-fe$county<-gsub("comanche `", "comanche", fe$county)
-fe$county<-gsub("bedford city", "bedford", fe$county)
-fe$county<-gsub("northumberland", "northumberlnd", fe$county)
-
-fe$county<-gsub("northwest arctic", "northwest artic borough", fe$county)
-fe$county<-gsub("o'brien", "obrien", fe$county)
-fe$county<-gsub("north slope", "north slope borouh", fe$county)
-fe$county<-gsub("mcclean", "mc lean", fe$county)
-
-fe$county<-gsub("mclennan", "mc lennan", fe$county)
-fe$county<-gsub("dolton", "cook", fe$county)
-
-fe$county<-gsub("mcdowell", "mc dowell", fe$county)
-fe$county<-gsub("mccook", "mc cook", fe$county)
-fe$county<-gsub("scotts bluff", "scott bluff", fe$county)
-fe$county<-gsub("prince of wales-hyder", "pr.of wales-hyder cns area", fe$county)
-fe$county<-gsub("sitka", "sitka borough", fe$county)
-fe$county<-gsub("mcdonald", "mc donald", fe$county)
-fe$county<-gsub("petersburg", "petersburg census area", fe$county)
-fe$county<-gsub("dewitt", "de witt", fe$county)
-fe$county<-gsub("st. mary's", "st. marys", fe$county)
-
-fe$county<-gsub("mcduffie", "mc duffie", fe$county)
-fe$county<-gsub("beford", "bedford", fe$county)
-fe$county<-gsub("mcminn", "mc minn", fe$county)
-fe$county<-gsub("st lucie", "st. lucie", fe$county)
-
-fe$county<-gsub("st joseph", "st. joseph", fe$county)
-fe$county<-gsub("st clair", "st. clair", fe$county)
-
-fe$county<-gsub("mccracken", "mc cracken", fe$county)
-fe$county<-gsub("mcleod", "mc leod", fe$county)
-fe$county<-gsub("acadiana", "acadia", fe$county)
-fe$county<-gsub("chicago", "cook", fe$county)
-fe$county<-gsub("do祓a ana", "dona ana", fe$county)
-fe$county<-gsub("leflore", "le flore", fe$county)
-
-fe$county<-ifelse(fe$city=="Sioux City", "woodbury", fe$county)
-fe$county<-ifelse(fe$city=="San Antonio", "bexar", fe$county)
-fe$county<-ifelse(fe$city=="Hermon", "penobscot", fe$county)
-fe$county<-ifelse(fe$city=="Ponca City", "kay", fe$county)
-fe$county<-ifelse((fe$city=="Danbury")&(fe$state=="CT"), "fairfield", fe$county)
-
-fe$county<-ifelse((fe$county=="bristol")&(fe$state=="VA"), "washington", fe$county)
-fe$county<-ifelse((fe$county=="mclean")&(fe$state=="IL"), "mc lean", fe$county)
-fe$county<-ifelse((fe$county=="stafford")&(fe$state=="NH"), "strafford", fe$county)
-fe$county<-ifelse((fe$city=="Nashville")&(fe$state=="GA"), "berrien", fe$county)
-fe$county<-ifelse((fe$county=="norfolk")&(fe$state=="VA"), "norfolk city", fe$county)
-fe$county<-ifelse((fe$city=="Alton")&(fe$state=="IL"), "madison", fe$county)
-fe$county<-ifelse((fe$city=="Bloomington")&(fe$state=="IL"), "mc lean", fe$county)
-fe$county<-ifelse((fe$city=="Richmond")&(fe$state=="IL"), "mc henry", fe$county)
-fe$county<-ifelse((fe$city=="El Cajon")&(fe$state=="CA"), "san diego", fe$county)
-fe$county<-ifelse((fe$city=="Hammond")&(fe$state=="IN"), "lake", fe$county)
-
-fe$county<-ifelse((fe$city=="Madison")&(fe$state=="WI"), "dane", fe$county)
+#### CHECK THAT THESE INDEX
+names(fe_new)<-
+  # c("id", "name", "age", "gender", "race", "image", "date", 
+  #            "address", "city", "state", "zip", "county", "agency", "cause",
+  #            "circumstance", "disposition", "news", "symptoms", "uniqueid",
+  #            "data-status", "date-descript", "dum", "dum2", "year")
 
 
-################################## MATCH COUNTIES WITH FIPS CROSSWALK
-crosswalk<-read.csv("ssa_fips_state_county2017.csv", stringsAsFactors = FALSE)%>%select(state, county, fipscounty)
-crosswalk$county<-tolower(crosswalk$county)
+fe_new<-fe_new%>%select()
+
+##### remap county names (don't still need this with FIPS CODES!!!!)
+# 
+# fe$county<-tolower(fe$county)
+# fe$county<-gsub("city of st. louis", "st. louis city", fe$county)
+# fe$county<-gsub("saint louis city", "st. louis city", fe$county)
+# fe$county<-gsub("st .louis", "st. louis city", fe$county)
+# fe$county<-gsub("city of st .louis", "st. louis city", fe$county)
+# 
+# fe$county<-gsub("lasalle", "la salle", fe$county)
+# fe$county<-gsub("prince georges", "prince george's", fe$county)
+# fe$county<-gsub("mcclennan", "mclennan", fe$county)
+# fe$county<-gsub("do単a ana", "dona ana", fe$county)
+# fe$county<-gsub("yukon koyukuk", "yukon-koyukuk", fe$county)
+# fe$county<-gsub("city of baltimore", "baltimore", fe$county)
+# fe$county<-gsub("charelston", "charleston", fe$county)
+# fe$county<-gsub("athens-clarke", "clarke", fe$county)
+# fe$county<-gsub("prince of wales hyder", "prince of wales-hyder", fe$county)
+# fe$county<-gsub("laporte", "la porte", fe$county)
+# fe$county<-gsub("saint joseph", "st. joseph", fe$county)
+# fe$county<-gsub("brooklyn", "kings", fe$county)
+# fe$county<-gsub("clallam bay", "clallam", fe$county)
+# fe$county<-gsub("saint mary's", "st. mary's", fe$county)
+# 
+# fe$county<-gsub("city of newport news", "newport news city", fe$county)
+# 
+# fe$county<-gsub("kuai", "kauai", fe$county)
+# fe$county<-gsub("shively", "jefferson", fe$county)
+# fe$county<-gsub("saint clair", "st. clair", fe$county)
+# fe$county<-gsub("clarl", "clark", fe$county)
+# fe$county<-gsub("park hill", "denver", fe$county)
+# fe$county<-gsub("braziria", "brazoria", fe$county)
+# fe$county<-gsub("andalusia", "covington", fe$county)
+# fe$county<-gsub("oskaloosa", "okaloosa", fe$county)
+# fe$county<-gsub("du page", "dupage", fe$county)
+# fe$county<-gsub("comanche `", "comanche", fe$county)
+# fe$county<-gsub("philadephia", "philadelphia", fe$county)
+# fe$county<-gsub("oglala lakota", "shannon", fe$county)
+# fe$county<-gsub("montrose county", "montrose", fe$county)
+# fe$county<-gsub("dekalb", "de kalb", fe$county)
+# fe$county<-gsub("prince george's", "prince georges", fe$county)
+# fe$county<-gsub("east baton rouge", "e. baton rouge", fe$county)
+# fe$county<-gsub("west baton rouge", "w. baton rouge", fe$county)
+# 
+# fe$county<-gsub("lasalle", "la salle", fe$county)
+# fe$county<-gsub("queen anne's", "queen annes", fe$county)
+# fe$county<-gsub("matanuska susitna", "matanuska-susitna", fe$county)
+# fe$county<-gsub("district of columbia", "the district", fe$county)
+# fe$county<-gsub("dc", "the district", fe$county)
+# 
+# fe$county<-gsub("st. john the baptist", "st. john baptist", fe$county)
+# fe$county<-gsub("desoto", "de soto", fe$county)
+# fe$county<-gsub("dupage", "du page", fe$county)
+# fe$county<-gsub("yukon koyukuk", "yukon-koyukuk", fe$county)
+# fe$county<-gsub("la paz", "lapaz", fe$county)
+# fe$county<-gsub("mcdonough", "mc donough", fe$county)
+# fe$county<-gsub("do単a ana", "dona ana", fe$county)
+# fe$county<-gsub("comanche `", "comanche", fe$county)
+# fe$county<-gsub("bedford city", "bedford", fe$county)
+# fe$county<-gsub("northumberland", "northumberlnd", fe$county)
+# 
+# fe$county<-gsub("northwest arctic", "northwest artic borough", fe$county)
+# fe$county<-gsub("o'brien", "obrien", fe$county)
+# fe$county<-gsub("north slope", "north slope borouh", fe$county)
+# fe$county<-gsub("mcclean", "mc lean", fe$county)
+# 
+# fe$county<-gsub("mclennan", "mc lennan", fe$county)
+# fe$county<-gsub("dolton", "cook", fe$county)
+# 
+# fe$county<-gsub("mcdowell", "mc dowell", fe$county)
+# fe$county<-gsub("mccook", "mc cook", fe$county)
+# fe$county<-gsub("scotts bluff", "scott bluff", fe$county)
+# fe$county<-gsub("prince of wales-hyder", "pr.of wales-hyder cns area", fe$county)
+# fe$county<-gsub("sitka", "sitka borough", fe$county)
+# fe$county<-gsub("mcdonald", "mc donald", fe$county)
+# fe$county<-gsub("petersburg", "petersburg census area", fe$county)
+# fe$county<-gsub("dewitt", "de witt", fe$county)
+# fe$county<-gsub("st. mary's", "st. marys", fe$county)
+# 
+# fe$county<-gsub("mcduffie", "mc duffie", fe$county)
+# fe$county<-gsub("beford", "bedford", fe$county)
+# fe$county<-gsub("mcminn", "mc minn", fe$county)
+# fe$county<-gsub("st lucie", "st. lucie", fe$county)
+# 
+# fe$county<-gsub("st joseph", "st. joseph", fe$county)
+# fe$county<-gsub("st clair", "st. clair", fe$county)
+# 
+# fe$county<-gsub("mccracken", "mc cracken", fe$county)
+# fe$county<-gsub("mcleod", "mc leod", fe$county)
+# fe$county<-gsub("acadiana", "acadia", fe$county)
+# fe$county<-gsub("chicago", "cook", fe$county)
+# fe$county<-gsub("do??a ana", "dona ana", fe$county)
+# fe$county<-gsub("leflore", "le flore", fe$county)
+# 
+# fe$county<-ifelse(fe$city=="Sioux City", "woodbury", fe$county)
+# fe$county<-ifelse(fe$city=="San Antonio", "bexar", fe$county)
+# fe$county<-ifelse(fe$city=="Hermon", "penobscot", fe$county)
+# fe$county<-ifelse(fe$city=="Ponca City", "kay", fe$county)
+# fe$county<-ifelse((fe$city=="Danbury")&(fe$state=="CT"), "fairfield", fe$county)
+# 
+# fe$county<-ifelse((fe$county=="bristol")&(fe$state=="VA"), "washington", fe$county)
+# fe$county<-ifelse((fe$county=="mclean")&(fe$state=="IL"), "mc lean", fe$county)
+# fe$county<-ifelse((fe$county=="stafford")&(fe$state=="NH"), "strafford", fe$county)
+# fe$county<-ifelse((fe$city=="Nashville")&(fe$state=="GA"), "berrien", fe$county)
+# fe$county<-ifelse((fe$county=="norfolk")&(fe$state=="VA"), "norfolk city", fe$county)
+# fe$county<-ifelse((fe$city=="Alton")&(fe$state=="IL"), "madison", fe$county)
+# fe$county<-ifelse((fe$city=="Bloomington")&(fe$state=="IL"), "mc lean", fe$county)
+# fe$county<-ifelse((fe$city=="Richmond")&(fe$state=="IL"), "mc henry", fe$county)
+# fe$county<-ifelse((fe$city=="El Cajon")&(fe$state=="CA"), "san diego", fe$county)
+# fe$county<-ifelse((fe$city=="Hammond")&(fe$state=="IN"), "lake", fe$county)
+# 
+# fe$county<-ifelse((fe$city=="Madison")&(fe$state=="WI"), "dane", fe$county)
+# 
+# 
+# ################################## MATCH COUNTIES WITH FIPS CROSSWALK
+# crosswalk<-read.csv("ssa_fips_state_county2017.csv", stringsAsFactors = FALSE)%>%select(state, county, fipscounty)
+# crosswalk$county<-tolower(crosswalk$county)
 
 ### ID non-matches
 # z<-(which(!(fe$county%in%crosswalk$county)))
@@ -155,12 +194,12 @@ fdat$cause<-ifelse(fdat$cause == "Asph", "Asphyxiated/Restrained",
              ifelse(fdat$cause == "Asphyxiated/restrained", "Asphyxiated/Restrained",
                     fdat$cause))
 
-#### create county index, then join. a straight join was producing duplicates
-c.index<-left_join(fdat%>%select(county, state), crosswalk)%>%rename(fips=fipscounty)%>%distinct()
-fdat<-left_join(fdat, c.index)
+# #### create county index, then join. a straight join was producing duplicates
+# c.index<-left_join(fdat%>%select(county, state), crosswalk)%>%rename(fips=fipscounty)%>%distinct()
+# fdat<-left_join(fdat, c.index)
 
-### manually remove duplicated case
-fdat<-fdat%>%filter(!(name=="Robert McAfee Jr"))
+### manually remove duplicated case ##### CHECK ON THIS!!!!!
+# fdat<-fdat%>%filter(!(name=="Robert McAfee Jr"))
 
 ### prep for surname matching
 names.tmp<-fdat%>%mutate(county=substrRight(fips,3))
@@ -186,6 +225,8 @@ missing<-c("\"\"Kenny\"\"", "1", "2", "police")
 
 tmp$surname<-ifelse(tmp$surname%in%missing, NA, names.tmp$surname)
 tmp$sex<-ifelse(tmp$gender=="Male", 0, ifelse(tmp$gender=="Female", 1, NA))
+
+
 
 name_out<-predict_race(voter.file=tmp%>%filter(race=="Race unspecified"), census.geo="county", 
                        census.key="518b6e66ffa1857a9e4ffd5d894f2934bb06c045")
