a636ff6e5bf10f9d349f526c1c03795364904c26
Michael Esposito <esposm2@uw.edu>
2018-03-06 17:32:03 -0800

    updated state models

--- states.r
+++ states.r
@@ -1,6 +1,6 @@
 #########################
 # Fatal Encounters Data
-# division main file, .r
+# states main file, .r
 # ... revision edits 
 # last edit 3/5 (by: ME)
 ########################
@@ -18,7 +18,7 @@ theme_set(theme_minimal())
 setwd("~/Projects/police_mort")
 select  = dplyr::select
 
-fe_new<-read_csv("./data/fatal-encounters-2-12-18.csv")
+fe_new<-read_csv("fatal-encounters-2-12-18.csv")
 fe_new<-fe_new%>%
   filter(`Date (Year)`>=2012)
 names(fe_new)<-c("id", "name", "age", "gender", "race", "URL", "death_date", 
@@ -39,7 +39,7 @@ fe_new<-fe_new%>%
          race = ifelse(race == "Native American/Alaskan", "other", race),
          race = ifelse(race == "Race unspecified", NA, race))
 
-imputeds<-read_csv("./data/predicted_race_05FalsePos.csv")
+imputeds<-read_csv("predicted_race_05FalsePos.csv")
 
 fdat <- imputeds %>% left_join(fe_new) # for surname/block imputation
 
@@ -54,7 +54,7 @@ fdat<-fdat%>%
   mutate(fips = ifelse(fips == 2270, 2158, fips))
 
 # ... attach CDC urban-rual scheme
-cdc = read_fwf(file = './data/NCHSURCodes2013.txt', 
+cdc = read_fwf(file = 'NCHSURCodes2013.txt', 
 			   fwf_positions(c(1, 3, 7, 10, 47, 98, 107, 116, 118, 120), 
 					  		 c(2, 5, 8, 45, 96, 105, 114, 116, 118, 120))
 		) %>%
@@ -79,14 +79,14 @@ regions = read.csv('regions.csv',
 		  dplyr::select(state, Division)
 
 # ... demographics
-pop2<-read_csv("./data/nhgis0029_ds216_20155_2015_county.csv")
+pop2<-read_csv("nhgis0029_ds216_20155_2015_county.csv")
 pop2$fips<-paste(pop2$STATEA, pop2$COUNTYA, sep="")
 pop2<-pop2%>%
   rename(fips.st=STATEA)%>%
   mutate(fips = as.numeric(fips), fips.st=as.numeric(fips.st)) %>%
   filter(fips.st!=72)
 
-cw<-read_csv("./data/fips-st-crosswalk.csv") %>%
+cw<-read_csv("fips-st-crosswalk.csv") %>%
   dplyr::select(state, fips)%>%rename(fips.st = fips)
 
 pop2<-left_join(pop2, cw)%>%
@@ -186,45 +186,153 @@ tmp = fdat %>%
 
 tmp2 = left_join(pop, tmp, 'fips') %>%
        mutate_at(vars(d.asian:d.total), funs(replace(., which(is.na(.)), 0))) %>%
-       tbl_df()
+       tbl_df() %>%
+       rename(division = Division)
 
 ## Modeling
 #  1. state level 
-# ... for blacks 
-blk.stan.0 = stan_glmer(d.black ~ (1 | ur.code) + (1 | state),
-                        prior_intercept = normal((log(0.94)-log(100000)), 2.5), 
-                        prior = normal(0, 2.5),
-                        prior_covariance = decov(1, 1, 1, 1),
-                        data = tmp2, 
-                        offset = I(log(black.men + 1)), 
-                        family ="neg_binomial_2", 
-                        iter = 2000, 
-                        chains = 4,
-                        adapt_delta = .999)
+# (running ur.code as fixed effect, becuase convergance tough at .999 if random)
+# ... for blacks     
+blk.stan.0 = stan_glmer(d.black ~ ur.code + (1|state),
+                      prior_intercept=normal((log(0.94)-log(100000)), 2.5), #for prior intercept, based on krieger estimates
+                      prior = normal(0, 2.5), #weakly informative, no difference from big urban
+                      prior_covariance = decov(1, 1, 1, 1), #default
+                      data = tmp2, offset=I(log(black.men+1)), 
+                      family="neg_binomial_2", iter=2000, chains=4)#, 
+                      #adapt_delta = 0.9999)
 
 # ... for whites 
-wht.stan.0 = stan_glmer(d.white ~ (1 | ur.code) + (1 | state),
-                        prior_intercept = normal((log(0.37)-log(100000)), 2.5),
-                        prior = normal(0, 2.5), 
-                        prior_covariance = decov(1, 1, 1, 1),
-                        data = tmp2, 
-                        offset = log(white.men), 
-                        family = "neg_binomial_2", 
-                        iter = 2000, 
-                        chains = 4,
-                        adapt_delta = .999)
-
+wht.stan.0 = stan_glmer(d.white ~ ur.code + (1|state),
+                      prior_intercept=normal((log(0.37)-log(100000)), 2.5), #for prior intercept, based on krieger estimates
+                      prior = normal(0, 2.5), #weakly informative, no difference from big urban
+                      prior_covariance = decov(1, 1, 1, 1), #default
+                      data = tmp2, offset=log(white.men), 
+                      family="neg_binomial_2", iter=2000, chains=4)#,
+                      #adapt_delta = 0.999)
 # ... for latinos
-lat.stan.0 = stan_glmer(d.latino ~ (1|ur.code) + (1 | division),
-                        prior_intercept = normal((log(0.37)-log(100000)), 2.5), 
-                        prior = normal(0, 2.5), 
-                        prior_covariance = decov(1, 1, 1, 1), 
-                        data = tmp2, 
-                        offset = I(log(latino.men + 1)), 
-                        family="neg_binomial_2", 
-                        iter = 2000, 
-                        chains = 4,
-                        adapt_delta = 0.999)
+lat.stan.0 = stan_glmer(d.latino ~ ur.code + (1|state),
+                      prior_intercept=normal((log(0.37)-log(100000)), 2.5), #for prior intercept, based on krieger estimates
+                      prior = normal(0, 2.5), #weakly informative, no difference from big urban
+                      prior_covariance = decov(1, 1, 1, 1), #default
+                      data = tmp2, offset=I(log(latino.men+1)), 
+                      family="neg_binomial_2", iter=2000, chains=4)#,
+                      #adapt_delta = 0.999)
+
+#save.image('bwl_fixed_mod_states.RData')
+
+
+## Visuals 
+# load('bwl_fixed_mod_states.RData')
+blk.stan<-blk.stan.0
+wht.stan<-wht.stan.0
+lat.stan<-lat.stan.0
+#tot.stan<-tot.stan.0
+tmp2<-tmp2%>%as.data.frame()
+
+# ...set up scenarios for prediction 
+ur.code  = unique(tmp2$ur.code)
+state = unique(tmp2$state)
+ur.state = expand.grid(state, ur.code); 
+names(ur.state) = c("state", "ur.code")
+n = length(state)*length(ur.code)
+
+# ... use 100,000,000 for offset to get enough trials
+# ... then rescale to rate per 100,000 
+newdata <- tmp2[1:n,]
+newdata$fips <- 0; newdata$tot.men<-newdata$black.men<-newdata$white.men<-newdata$latino.men<-100000000; 
+newdata$ur.code<-ur.state$ur.code; newdata$state<-ur.state$state
+
+# ... estimate predictive posterior for each group 
+# ... black
+post.blk = posterior_predict(blk.stan, newdata = newdata)/1000
+post.blk.sims<-cbind(ur.state, t(post.blk))
+post.blk.sims<-gather(post.blk.sims, 
+                      key=sim, 
+                      value=sim.rate, 
+                      -ur.code, -state)
+
+# ... white
+post.wht = posterior_predict(wht.stan, newdata = newdata)/1000
+post.wht.sims<-cbind(ur.state, t(post.wht))
+post.wht.sims<-gather(post.wht.sims, 
+                      key=sim, 
+                      value=sim.rate, 
+                      -ur.code, -state)
+
+# ... latino
+post.lat = posterior_predict(lat.stan, newdata = newdata)/1000
+post.lat.sims<-cbind(ur.state, t(post.lat))
+post.lat.sims<-gather(post.lat.sims, 
+                      key=sim, 
+                      value=sim.rate, 
+                      -ur.code, -state)
+
+# 3: calculate mean and 95% uncertainty intervals for each group 
+post.blk.int = bind_cols(ur.state, as.data.frame(
+  t(apply(post.blk, 2, function(x)quantile(x, probs=c(0.025, 0.5, 0.975))))))
+post.blk.int = post.blk.int %>% mutate(race = 'Black')
+
+post.wht.int = bind_cols(ur.state, as.data.frame(
+  t(apply(post.wht, 2, function(x)quantile(x, probs=c(0.025, 0.5, 0.975))))))
+post.wht.int = post.wht.int %>% mutate(race = 'White')
+
+post.lat.int = bind_cols(ur.state, as.data.frame(
+  t(apply(post.lat, 2, function(x)quantile(x, probs=c(0.025, 0.5, 0.975))))))
+post.lat.int = post.lat.int %>% mutate(race = 'Latino')
+
+# ... bind everything together & get ready for plotting
+post.estimates = bind_rows(post.blk.int, post.wht.int, post.lat.int);
+colnames(post.estimates)[3:5] = c('lwr', 'estimate', 'upr')
+
+p.dat1 = post.estimates %>%
+     rename(rate = estimate) %>% 
+     mutate(type = 'estimated', 
+        ur.code  = as.character(ur.code), 
+        state = as.character(state)) %>%
+     select(ur.code, state, rate, race, type) 
+
+
+##########
+# ... add in divisons, for plotting
+hld = tmp2 %>% 
+      select(state, division) %>%
+      distinct()
+
+p.dat2 = left_join(p.dat1, hld, 'state')
+
+# ... also add in marker for if (state, ur.code) combo exist in data
+a = tmp2 %>% 
+    select(state, ur.code) %>%
+    distinct() %>%
+    mutate(state_code = paste(state, ur.code, sep = '-')) %>%
+    select(state_code) 
+
+b = p.dat2 %>%
+    mutate(state_code = paste(state, ur.code, sep = '-')) %>%
+    tbl_df() 
+
+p.dat3 = b %>% 
+         mutate(real = ifelse((b$state_code %in% a$state_code) == TRUE, 1, 0))
+
+ggplot(p.dat3 %>% filter(ur.code == 'medium urban', real == 1), 
+       aes(x = reorder(state, rate), y = rate, fill = race, group = race)) + #, #ymin = `2.5%`, ymax = `97.5%`)) + 
+    geom_point(color = 'grey10', pch = 21, size = 2.5, alpha = .75) + 
+    coord_flip() + 
+    facet_wrap(~ur.code) +
+    scale_fill_brewer(palette = 'Set2') + 
+    facet_wrap(~division, scale = 'free_y')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -333,7 +441,7 @@ scale_fill_brewer(palette = 'Paired')
 
 # ... or 
 # ... unscaled! 
-ggplot(post.blk.int3 %>% filter(ur.code == 'medium urban', real == 1), 
+ggplot(post.blk.int2 %>% filter(ur.code == ''), 
        aes(x = reorder(state, `50%`), y = `50%`, fill = Division)) + #, #ymin = `2.5%`, ymax = `97.5%`)) + 
     geom_point(color = 'grey10', pch = 21, size = 2.5) + 
     coord_flip() + 
